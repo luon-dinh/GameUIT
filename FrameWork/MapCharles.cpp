8c0af351f182fa2360f5dc9ce7d796ff6f5b09b9
mirushi <luubieunghi@gmail.com>
2019-07-15 14:09:26 +0700

    Add animation

---
+++ FrameWork/MapCharles.cpp
@@ -0,0 +1,6 @@
+#include "MapCharles.h"
+
+MapCharles::MapCharles(const char * imgPath, const char * txtPath, const char * mapObjectPath, const char * waterTopPath, const char * waterBottomPath, const char * sewerPath) : GameMap(imgPath, txtPath, mapObjectPath)
+{
+	//GameMap::GameMap(imgPath, txtPath, mapObjectPath);
+}
