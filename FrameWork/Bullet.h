60052bcccbc71348bdd9499d480f99a36b768314
Daiyamondo <45528757+Daiyamondo2018@users.noreply.github.com>
2019-07-24 18:35:58 +0700

    Add enemy

---
+++ FrameWork/Bullet.h
@@ -0,0 +1,45 @@
+#pragma once
+#include"Object.h"
+#include"Camera.h"
+#include"Player.h"
+class Bullet:public Object {
+public:
+	float speed;
+	Animation* animation;
+	Animation* animationExplode;
+	Player::MoveDirection direction;
+	float existTime;
+
+	Bullet()
+	{
+		animationExplode = new Animation(Tag::BULLETEXPLODE, 0, 3);
+	}
+	~Bullet()
+	{
+
+	}
+	virtual void Update(float dt)
+	{
+
+	};
+	virtual void Render()
+	{
+		if (!this->GetActive())
+			return;
+		D3DXVECTOR3 pos = Camera::getCameraInstance()->convertWorldToViewPort(D3DXVECTOR3(this->pos));
+		switch (this->direction) {
+		case Player::MoveDirection::LeftToRight:
+			animation->Render(D3DXVECTOR2(pos), TransformationMode::FlipHorizontal);
+			break;
+		case Player::MoveDirection::RightToLeft:
+			animation->Render(pos);
+			break;
+		default:
+			break;
+		}
+	};
+	virtual void OnCollision(Object* object, collisionOut* colOut)
+	{
+
+	}
+};
--- FrameWork/Solder.h
+++ FrameWork/Solder.h
@@ -1,16 +1,24 @@
 #pragma once
-#include "Enemy.h"
-
+#include "Enemy.h""
+#include"Shield.h"
+#include"BulletManager.h"
 
 class Solder : public Enemy {
 private:
 public:
-	Solder(EnemyType type);
+	Solder(bool movable);
 	~Solder();
+	State stateName;
+	Bullet* bullet;
+	bool movable;
+	float timeCurrentState;
+	std::unordered_map<State, Animation*> animations;
 
-
-	void Move() override;
+	void LoadAllAnimation();
+	void ChangeState(State state)override; 
 	void Shoot() override;
 	void OnCollision(Object* object, collisionOut* colOut) override;
-
+	void Update(float dt) override;
+	void Respawn()override {};
+	void Render();
 };
