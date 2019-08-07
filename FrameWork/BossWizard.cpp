b6c7ec8fe076edd90a3bcb991845567d007a8c26
Daiyamondo <45528757+Daiyamondo2018@users.noreply.github.com>
2019-08-07 14:10:03 +0700

    ...

--- FrameWork/BossWizard.cpp
+++ FrameWork/BossWizard.cpp
@@ -412,7 +412,7 @@ void BossWizard::LoadAllAnimations()
 	animations[State::STAND_SMILE] = new Animation(Tag::BOSSWIZARD, 0, 3, 120/3);
 	animations[State::RUNNING] = new Animation(Tag::BOSSWIZARD, 3, 7);
 	animations[State::DEAD] = new Animation(Tag::BOSSWIZARD, 8, 10);
-	animations[State::ATTACK] = new Animation(Tag::BOSSWIZARD, 10, 13, 800/3);
+	animations[State::ATTACK] = new Animation(Tag::BOSSWIZARD, 10, 13);
 	animations[State::FLYING] = new Animation(Tag::BOSSWIZARD, 13, 16);
 	animations[State::STAND_PUNCH] = new Animation(Tag::BOSSWIZARD, 16, 18);
 	animations[State::BEATEN] = new Animation(Tag::BOSSWIZARD, 7, 8);
--- FrameWork/BossWizardShootingState.h
+++ FrameWork/BossWizardShootingState.h
@@ -3,7 +3,7 @@
 #include"BossWizard.h"
 class BossWizardShootingState :public BossWizardState {
 public:
-	const float maxTimeShoot = 800;
+	const float maxTimeShoot = 500;
 	const float shoot1 = 30;// deltay so với player để set hướng đạn bay
 	const float shoot2 = 80;
 	float timeShoot;
